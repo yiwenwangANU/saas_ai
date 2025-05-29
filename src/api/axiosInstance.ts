357225513bda4a7da50e19ab0a6a24513c27cbc4
Yiwen Wang <wangyiwenau@gmail.com>
2025-05-29 10:23:52 +1000

    add <ToastContainer /> component in app

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { ModalProvider } from "./contexts/ModalContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <ModalProvider>
           <App />
+          <ToastContainer />
         </ModalProvider>
       </QueryClientProvider>
     </BrowserRouter>
