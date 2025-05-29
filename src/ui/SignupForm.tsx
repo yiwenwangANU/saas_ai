e2f391d699045be829b06c94ce00b974827c4f1f
Yiwen Wang <wangyiwenau@gmail.com>
2025-05-29 10:03:43 +1000

    add name field in signup form

--- src/ui/SignupForm.tsx
+++ src/ui/SignupForm.tsx
@@ -5,6 +5,7 @@ import useSignup from "../hooks/useSignup";
 type Inputs = {
   email: string;
   password: string;
+  name: string;
 };
 const Signup = () => {
   const {
@@ -43,6 +44,13 @@ const Signup = () => {
           {/* errors will return when field validation fails  */}
           {errors.password && <span>This field is required</span>}
 
+          <label>Name</label>
+          <input
+            {...register("name", { required: true })}
+            className="border-green-300 border-2 rounded-xl px-1  focus:border-green-300 focus:outline-none"
+          />
+          {/* errors will return when field validation fails  */}
+          {errors.name && <span>This field is required</span>}
           <div className="flex justify-end pt-3">
             <Button variant="login">Create User</Button>
           </div>
