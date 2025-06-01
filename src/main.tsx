c0d29a74dc2b30584319a0bbc6124f02a91c9f83
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-01 11:50:27 +1000

    wrap app with authprovider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router";
 import { ModalProvider } from "./contexts/ModalContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -12,10 +13,12 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <App />
-          <ToastContainer />
-        </ModalProvider>
+        <AuthProvider>
+          <ModalProvider>
+            <App />
+            <ToastContainer />
+          </ModalProvider>
+        </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
