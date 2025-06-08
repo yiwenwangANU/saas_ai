209c72ef1c9cc0041b4ce0a8a2e84e00944a23a1
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-08 11:31:14 +1000

    change mealplan page render logic

--- src/pages/MealPlan.tsx
+++ src/pages/MealPlan.tsx
@@ -1,10 +1,19 @@
 import { Navigate } from "react-router";
 import { useAuthContext } from "../contexts/AuthContext";
+import { useModalContext } from "../contexts/ModalContext";
+import SignupLoginList from "../ui/SignupLoginList";
+import { useEffect } from "react";
 
 const MealPlan = () => {
   const { isLoggin } = useAuthContext();
-  if (isLoggin) return <div>MealPlan Page</div>;
-  else return <Navigate to="/" replace />;
+  const { handleOpenModal } = useModalContext();
+
+  useEffect(() => {
+    if (!isLoggin) handleOpenModal(<SignupLoginList />);
+  }, [isLoggin, handleOpenModal]);
+
+  if (!isLoggin) return <Navigate to="/" replace />;
+  else return <div>MealPlan Page</div>;
 };
 
 export default MealPlan;
