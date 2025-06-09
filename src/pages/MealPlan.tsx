2799e4122089d14b20672081f42312ef86432372
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-09 10:47:03 +1000

    update redirect logic for mealplan page

--- src/pages/MealPlan.tsx
+++ src/pages/MealPlan.tsx
@@ -1,17 +1,8 @@
 import { Navigate } from "react-router";
 import { useAuthContext } from "../contexts/AuthContext";
-import { useModalContext } from "../contexts/ModalContext";
-import SignupLoginList from "../ui/SignupLoginList";
-import { useEffect } from "react";
 
 const MealPlan = () => {
   const { isLoggin } = useAuthContext();
-  const { handleOpenModal } = useModalContext();
-
-  useEffect(() => {
-    if (!isLoggin) handleOpenModal(<SignupLoginList />);
-  }, [isLoggin, handleOpenModal]);
-
   if (!isLoggin) return <Navigate to="/" replace />;
   else return <div>MealPlan Page</div>;
 };
