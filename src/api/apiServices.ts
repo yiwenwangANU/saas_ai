9106ac65b28e072e37347e6a2b5cb72426eff024
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-09 16:04:39 +1000

    create axios api service for stripe subscritption

--- src/api/apiServices.ts
+++ src/api/apiServices.ts
@@ -1,1 +1,32 @@
+import axios from "axios";
+import { axiosPublic } from "./axiosInstance";
 
+// Subscribe user
+type SubscribeData = {
+  email: string;
+  priceId: string;
+};
+
+type SubscribeResponse = {
+  sessionId: string;
+};
+
+export const signupUser = async (
+  subscribeData: SubscribeData
+): Promise<SubscribeResponse> => {
+  // send json data this time
+  try {
+    const response = await axiosPublic.post<SubscribeResponse>(
+      `/api/stripe/create-checkout-session`,
+      subscribeData
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.message);
+    } else if (error instanceof Error) {
+      console.error("Error:", error.message);
+    }
+    throw error; // Rethrow for error handling in components
+  }
+};
