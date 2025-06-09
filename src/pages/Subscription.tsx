b059f48c1b555b04b8c6f715209d099e169ebc80
Yiwen Wang <wangyiwenau@gmail.com>
2025-06-09 12:21:37 +1000

    refine css and add more content to subscription page

--- src/pages/Subscription.tsx
+++ src/pages/Subscription.tsx
@@ -4,14 +4,14 @@ import Button from "../ui/Button";
 const Subscription = () => {
   return (
     <div>
-      <div className="flex flex-col gap-4 items-center mx-20 rounded-xl py-10 my-10">
+      <div className="flex flex-col gap-5 items-center mx-20 rounded-xl py-10 my-10">
         <div className="text-6xl font-bold">Price</div>
-        <div className="text-2xl pb-20">
+        <div className="text-2xl pb-20 font">
           Get started on our weekly plan or upgrade to monthly or yearly when
           you're ready.
         </div>
         <div className="flex flex-row gap-10 items-center px-10">
-          <div className="border-2 border-gray-200 rounded-3xl shadow-xl flex flex-col gap-10 px-14 pt-7 pb-10">
+          <div className="border-2 border-gray-200 rounded-3xl shadow-xl flex flex-col gap-10 px-14 pt-7 pb-10 hover:scale-[1.02] duration-100">
             <div className="text-2xl font-bold">Weekly Plan</div>
             <div>
               <span className="text-7xl font-bold">$9.99</span>
@@ -36,7 +36,7 @@ const Subscription = () => {
             </ul>
             <Button variant="subscribe">Subscribe Weekly Plan</Button>
           </div>
-          <div className="border-2 border-gray-200 rounded-3xl shadow-xl flex flex-col gap-10 px-14 pt-7 pb-10">
+          <div className="border-2 border-gray-200 rounded-3xl shadow-xl flex flex-col gap-10 px-14 pt-7 pb-10 hover:scale-[1.02] duration-100">
             <div className="text-2xl font-bold">Monthly Plan</div>
             <div>
               <span className="text-7xl font-bold">$39.99</span>
@@ -61,7 +61,7 @@ const Subscription = () => {
             </ul>
             <Button variant="subscribeMonthly">Subscribe Monthly Plan</Button>
           </div>
-          <div className="border-2 border-gray-200 rounded-3xl shadow-xl flex flex-col gap-10 px-14 pt-7 pb-10">
+          <div className="border-2 border-gray-200 rounded-3xl shadow-xl flex flex-col gap-10 px-14 pt-7 pb-10 hover:scale-[1.02] duration-100">
             <div className="text-2xl font-bold">Yearly Plan</div>
             <div>
               <span className="text-7xl font-bold">$299.99</span>
